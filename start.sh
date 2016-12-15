3289dcb4333165b06ebdc1575234156f161c3975
Sorin Buliarca <Sorin.Buliarca@iquestgroup.com>
2016-12-15 19:28:47 +0200

    Testing accessing Kubernetes secrets.

--- start.sh
+++ start.sh
@@ -1,25 +1,5 @@
 #!/bin/sh
 
-authinit() {
-	for i in 0 1 2 3 4 5 6 7 8 9
-	do
-		curl -f -s "$ETCD_HOST/v2/keys$ETCD_CONFIG_KEY" >/tmp/t
-		# Check the file is non-zero size
-		if [ -s /tmp/t ]
-		then
-			log "Got value for $ETCD_CONFIG_KEY, setting /.htpasswd"
-			cat /tmp/t | jq -r .node.value | tr ',' '\n' > /.htpasswd;
-			rm /tmp/t;
-			return
-		else
-			log "Failed fetching $ETCD_CONFIG_KEY from etcd ($i)"
-			sleep 5
-			continue
-		fi
-	done
-	log "Failed fetching $ETCD_CONFIG_KEY from etcd, starting with empty '/.htpasswd'"
-}
-
 shutdown() {
 	log "Stopping"
 	pkill varnishd
@@ -31,7 +11,6 @@ shutdown() {
 
 watch() {
 	while true; do
-		curl -f -s "$ETCD_HOST/v2/keys$ETCD_CONFIG_KEY?wait=true" >/tmp/t && cat /tmp/t | jq -r .node.value | tr ',' '\n' > /.htpasswd;
 		# sleep in case val doesn't exist/etcd not running etc
 		sleep 5;
 	done
@@ -44,7 +23,7 @@ log() {
 trap 'shutdown' HUP INT QUIT KILL TERM
 
 # Try and get intial auth value
-authinit
+#authinit
 
 # Convert environment variables in the conf to fixed entries
 for name in VARNISH_BACKEND_PORT VARNISH_BACKEND_HOST HOST_HEADER CONTENT_NOTIFICATIONS_PUSH_PORT LIST_NOTIFICATIONS_PUSH_PORT
