16425fcb932939fb8a0fea2c96b1160467350adf
Sorin Buliarca <sorin.buliarca@ft.com>
2017-09-01 11:21:55 +0300

    Fixed allowing inline C.
    (cherry picked from commit 7ef444e)

--- start.sh
+++ start.sh
@@ -17,7 +17,7 @@ trap 'shutdown' HUP INT QUIT KILL TERM
 
 # Start varnish and log
 log "Starting"
-varnishd -f /etc/varnish/default.vcl -s malloc,1024m -t 5 -p default_grace=0 &
+varnishd -pvcc_allow_inline_c=true -f /etc/varnish/default.vcl -s malloc,1024m -t 5 -p default_grace=0 &
 sleep 4
 
 varnishncsa -F '%{X-Forwarded-For}i %u %{%d/%b/%Y:%T}t %U%q %s %D "%{User-Agent}i" transaction_id=%{X-Request-Id}i %{Varnish:handling}x' &
